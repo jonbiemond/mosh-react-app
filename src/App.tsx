a61bc684f40e752b54b2294a4521bd62f65b221c
Jonathan Biemond <jonbiemond@gmail.com>
2023-07-23 00:52:39 +0000

    feat: add ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>
+  let items = ["Vancouver", "Toronto", "Calgary", "Montreal", "Tokyo"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
