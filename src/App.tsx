fd5d7b1faaed0b18ec1b1e7c639cfb361ced723c
Jonathan Biemond <jonbiemond@gmail.com>
2023-07-23 01:42:57 +0000

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  let items = ["Vancouver", "Toronto", "Calgary", "Montreal", "Tokyo"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
+  const handleClickButton = (name: string) => {
+    console.log(name);
   };
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <Button onClick={handleClickButton}>My Button</Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: (name: string) => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button
+      type="button"
+      className={"btn btn-" + color}
+      onClick={() => {
+        onClick(children);
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
